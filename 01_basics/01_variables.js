88940285027c0b784e38f3d30d1910ce30c6a5a8
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-08-08 15:40:20 +0000

    3rd commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId=144553
 let accountEmail="shaif@gmail.com"
 var accountPassword="12345"
 accountCity="ernakulam"
-
+let accountState;
 /*
 Preferred not to use var
 because of issue in block scope and functional scope
@@ -12,7 +12,7 @@ accountEmail="khan@gmail.com"
 accountPassword="2121"
 accountCity="kochi"
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 //accountId=2 //Not allowed
 console.log(accountId);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer  version
+// alert("hello") //we are using nodeJs, not Browser
+
+console.log(3+3) //Code readability should be high
+console.log("hello")
+
+let name="shaif";
+let age=18;
+let isLoggedIn=false;
+//primitive Data Types
+// number => 2 to power 53
+// bigInt => 
+// String => ""
+// boolean=> true/false
+// null => standalone  value
+// undefined => value is not assined
+// symbol => => unique
+
+
+//Object
+
+
+console.log(typeof undefined) //undefined
+console.log(typeof undefined) // op:- object
