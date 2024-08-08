bcf4c65795c738215bd336150d95f31600cb76dd
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-08-08 16:02:42 +0000

    4th  commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score=33
+
+//let score="33abc"
+//let score=null
+//let score=undefined
+let  score=true
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+//let isLoggedIn=1
+let isLoggedIn="hi"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// "" => false; 
+// "abc"=> true
+
+let sumNumber=33
+let stringNumber=String(sumNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
