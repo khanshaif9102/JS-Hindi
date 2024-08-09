01d2395488f1f6180b6b3c829957a9ce357ec026
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-08-09 10:17:40 +0000

    5th commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,12 +3,12 @@
 //let score="33abc"
 //let score=null
 //let score=undefined
-let  score=true
+let score = true
 
 console.log(typeof score)
 console.log(typeof (score))
 
-let valueInNumber=Number(score)
+let valueInNumber = Number(score)
 
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
@@ -18,9 +18,9 @@ console.log(valueInNumber)
 // true => 1; false=> 0
 
 //let isLoggedIn=1
-let isLoggedIn="hi"
+let isLoggedIn = "hi"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(typeof booleanIsLoggedIn)
 console.log(booleanIsLoggedIn)
@@ -29,8 +29,31 @@ console.log(booleanIsLoggedIn)
 // "" => false; 
 // "abc"=> true
 
-let sumNumber=33
-let stringNumber=String(sumNumber)
+let sumNumber = 33
+let stringNumber = String(sumNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//===========================Operations===============================
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 3)
+console.log(2 % 3)
+
+let str1 = "hello"
+let str2 = " shaif"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log((3 + 4) * 5 % 3)
