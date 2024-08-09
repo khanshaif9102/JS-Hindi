60c2cfd33072298a1b7b7fe205907847133bfa92
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-08-09 12:05:36 +0000

    6th commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -57,3 +57,14 @@ console.log("1" + 2 + 2)
 console.log(1 + 2 + "2")
 
 console.log((3 + 4) * 5 % 3)
+
+console.log(+true)
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2;
+console.log(num1)
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter++)
