21a7a8a9ab489b9b8e68a76320da04751454054d
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-08-11 14:13:54 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score=400;
+const balance=new Number(500);
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum=23.8966;
+// console.log(otherNum.toPrecision(5));
+
+const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log();
+
+//==========================Maths==================================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(8.585));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(100));
+
+// console.log(Math.min(4,3,5,8,1,25))
+// console.log(Math.max(1,5,3,5,74,5,222,14,55));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
