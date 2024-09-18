653d04b0224906da411d7e74f00ec314d84c0e9d
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-09-18 06:04:39 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,15 +1,54 @@
 
 var c=300
 let a=300
-if(true){
-    let a=10
-    const b=20
-     c=30
-     console.log("Inner a",a);
+// if(true){
+//     let a=10
+//     const b=20
+//      c=30
+//      console.log("Inner a",a);
      
+// }
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username="shaif"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+// if(true){
+//     const username="shaif"
+//     if(username=="shaif"){
+//         const website=" youtube"
+//         console.log(username+ website);
+//     }
+//     // console.log(website);
+// }
+
+// console.log(username);
+
+// +++++++++++++++ Interesting +++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1;
 }
 
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+addTwo(5);
+const addTwo=function(num){
+    return num+2
+}
+
