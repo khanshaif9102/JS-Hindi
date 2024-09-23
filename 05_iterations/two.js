fc11a8746093c5e1428850e230663920524b8089
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-09-23 12:46:18 +0000

    while amd do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let i=0;
+// while(i<=10){
+//     console.log(`value of i is ${i}`);
+//     i++;
+// }
+
+let myArray=["flash","superman","batman"]
+let i=0;
+while(i<myArray.length){
+    console.log(`value is ${myArray[i]}`);
+    i++;
+}
+i=0;
+do{
+    console.log(`value is ${myArray[i]}`);
+    i++;
+} while(i<myArray.length)
+
+    do{
+        console.log("hi");
+    }while(false)
