d96b608def75a6b04e982e1011de4751a5234a85
MD SHAIFULLAH KHAN <138392151+khanshaif9102@users.noreply.github.com>
2024-10-06 12:17:33 +0000

    unlimited color project

---
+++ Projects/UnlimitedColors/script.js
@@ -0,0 +1,29 @@
+//Generate a random color
+
+const randomColor=function(){
+    const hex="0123456789ABCDEF";
+    let color='#';
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+let setIntervalId;
+// console.log(randomColor());
+const startChangingColor=function(){
+    //document.body.style.backgroundColor=randomColor();
+  if(!setIntervalId){
+    setIntervalId= setInterval(changeBgColor,1000)
+  }
+    function changeBgColor(){
+        document.body.style.backgroundColor=randomColor();
+    }
+}
+const stopChangingColor=function(){
+    clearInterval(setIntervalId)
+    setIntervalId=null;
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
